f464834653690aaef639ea3a114bc059b1e89b2e
Timon Vonk <mail@timonv.nl>
2024-04-12 10:36:20 +0200

    feat/agent simple list files (#261)
    * feat: supported languages as primary citizen
    
    * feat: Allow skipping codebase indexing
    
    * fix(fluyt): Properly use git ls-files
    
    * feat: substitute arguments in after hooks and stream if websocket

--- src/codebase.rs
+++ src/codebase.rs
@@ -1,9 +1,23 @@
+use infrastructure::supported_languages::SupportedLanguages;
+
 #[derive(Debug, Clone)]
 pub struct Codebase {
     pub name: String,
     pub url: String,
-    pub lang: String,
+    pub lang: SupportedLanguages,
     pub working_dir: Option<String>,
     pub test_command: Option<String>,
     pub coverage_command: Option<String>,
+}
+
+static RUST_EXTENSIONS: &[&str] = &["rs"];
+static TYPESCRIPT_EXTENSIONS: &[&str] = &["ts"];
+
+impl Codebase {
+    pub fn supported_extensions(&self) -> &[&str] {
+        match self.lang {
+            SupportedLanguages::Rust => RUST_EXTENSIONS,
+            SupportedLanguages::Typescript => TYPESCRIPT_EXTENSIONS,
+        }
+    }
 }
